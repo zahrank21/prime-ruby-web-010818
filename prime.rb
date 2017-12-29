360d9f4034b77ff9ebee016db68e8226d3a57566
zahrank21 <zahrank@gmail.com>
2017-12-29 18:19:17 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(int)
+  result = []
+  counter = 1
+if int <= 1
+  return false
+end
+  while counter <= int
+    if int % counter == 0
+      result << counter
+    end
+    counter += 1
+  end
+  if result.length > 2
+    return false
+  else
+    return true
+  end
+end
